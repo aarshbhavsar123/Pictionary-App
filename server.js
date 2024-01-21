2fc0087277c1389789c3f9ea33cd30d8e2a20525
Aarsh <202101474@daiict.ac.in>
2024-01-21 13:42:44 +0530

    changes

--- server.js
+++ server.js
@@ -8,33 +8,31 @@ const io = socketIO(server);
 
 const PORT = process.env.PORT || 3000;
 
-// Serve the HTML file
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-// Dictionary to store connected clients and their usernames
+
 const clients = {};
 
 io.on('connection', (socket) => {
 
-    // Handle the event when a new user joins
+    // console.log("User Connected to the server");
     socket.on('join', (data) => {
         const { roomId, username } = data;
         socket.join(roomId);
         clients[socket.id] = { username, roomId };
 
-        // Notify all clients in the room about the new user
         io.to(roomId).emit('chat message', `${username} has joined the chat`);
     });
 
-    // Handle the event when a user sends a message
     socket.on('chat message', (msg) => {
         const { username, roomId } = clients[socket.id];
         io.to(roomId).emit('chat message', `${username}: ${msg}`);
     });
 
-    // Handle the event when a user disconnects
+    
     socket.on('disconnect', () => {
         const { username, roomId } = clients[socket.id];
         io.to(roomId).emit('chat message', `${username} has left the chat`);
@@ -43,7 +41,7 @@ io.on('connection', (socket) => {
     });
 });
 
-// Start the server
+
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
